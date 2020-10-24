02610f43d1de3156bdcb82c92c2780d22ae4ade2
mehedi-2002 <72048371+mehedi-2002@users.noreply.github.com>
2020-10-24 16:28:30 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,7 @@
 function buying() {
     alert("GREAT CHOICE! ----- UNFORTUNATLY NO PRODUCTS ARE CURRENTLY AVAILABLE");
 }
+
+function social_media() {
+    alert("Thank you for your interest in joining us on social media, right now it's not available, but check back shortly!");
+}
